b50cd77c78db2567228f14a1d8d507ba1af40f30
Roman <github@roman-kosov.ru>
2018-05-06 15:32:47 +0300

    add new

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Initial OS X setup
 
 ```
-bash -c "`curl -L https://raw.githubusercontent.com/andrew--r/osx-setup/master/setup.sh`"
+bash -c "`curl -L https://raw.githubusercontent.com/roman-kosov/osx-setup/master/setup.sh`"
+```
+
+# VS Code config
+
+```
+{
+    "editor.fontFamily": "Fira Code",
+    "editor.fontLigatures": true,
+    "workbench.colorTheme": "Dracula",
+    "editor.fontSize": 14,
+    "prettier.tabWidth": 4
+}
 ```
--- setup.sh
+++ setup.sh
@@ -14,7 +14,13 @@ brew install git
 brew install wget
 brew install jq # rich json viewing
 brew install httpie # frendlier curl alternative
-brew install tldr # friendlier man alternative 
+brew install tldr # friendlier man alternative
+
+# Mac App Store
+brew install mas # command line interface
+mas install 1284863847 # Unsplash Wallpapers
+mas install 497799835 # Xcode
+mas install 1176624652 # Translatium https://github.com/quanglam2807/translatium
 
 brew tap caskroom/cask
 brew tap caskroom/versions
@@ -24,9 +30,7 @@ function installcask() {
 }
 
 # random stuff
-installcask skype
 installcask telegram-desktop
-installcask slack
 installcask day-o # enhances default time widget in topbar
 installcask teamviewer
 installcask ccleaner
@@ -36,24 +40,24 @@ installcask libreoffice
 installcask cakebrew
 installcask spectacle
 
+# other
+installcask spotify
+installcask toggl # tracking system
+installcask notion
+installcask imageoptim
+installcask github # gui
+installcask astrill # vpn
+
 # browsers
-installcask google-chrome
-installcask opera
-installcask firefox
+installcask chromium
 installcask firefoxdeveloperedition
 
 # editors
-installcask sublime-text
-installcask atom
 installcask visual-studio-code
 
 # devtools
 installcask iterm2
-installcask virtualbox
-installcask vagrant
-installcask tunnelblick
 installcask filezilla
-installcask zeplin
 
 # install powerline fonts
 mkdir ~/tmp && cd ~/tmp
@@ -61,6 +65,11 @@ git clone https://github.com/powerline/fonts.git
 cd fonts
 sh -c ./install.sh
 cd ~/tmp && rm -rf ./fonts
+
+# Fira Code: monospaced font with programming ligatures: https://github.com/tonsky/FiraCode
+# VS Code Instructions: https://github.com/tonsky/FiraCode/wiki/VS-Code-Instructions
+brew tap caskroom/fonts
+brew cask install font-fira-code
 
 # Install Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
