938d3998ea3293ac12e5f06025eb489f6c8b423d
Roman Kosov <11503318+91k@users.noreply.github.com>
2024-08-10 20:40:40 +0300

    fix: Обновляет список нужный приложений для чистой установки

--- setup.sh
+++ setup.sh
@@ -1,88 +1,56 @@
 #!/bin/sh
 
-# install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-# install brew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
 brew upgrade
 
-brew install tree
+# git
 brew install git
-brew install wget
-brew install jq # rich json viewing
-brew install httpie # frendlier curl alternative
-brew install tldr # friendlier man alternative
 
-# Mac App Store
-brew install mas # command line interface
+# CLI for Mac App Store
+brew install mas
 mas install 497799835 # Xcode
+mas install 946399090 # telegram lite
 
-brew tap caskroom/cask
-brew tap caskroom/versions
-
-function installcask() {
-  brew cask install "${@}" 2> /dev/null
+# install apps via homebrew
+function install_app() {
+    brew install --cask "${@}" 2> /dev/null || brew install "${@}" 2> /dev/null
 }
 
-# random stuff
-installcask telegram-desktop
-installcask day-o # enhances default time widget in topbar
-installcask teamviewer
-installcask vlc
-installcask transmission
-installcask cakebrew
+brew_apps=(
+    "github" # gui version
+    "firefox"
+    "postman"
+    "visual-studio-code"
+    "discord"
+    "openvpn-connect"
 
-# other
-installcask spotify
-installcask github # gui
+    "warp"
+    "rectangle" # window manager
+    "raycast"
+    "figma"
+    "yandex-music"
+    "adguard-vpn"
+    "chatgpt"
+    "vlc"
+    "zoom"
+    "font-hack-nerd-font"
+)
 
-# browsers
-installcask google-chrome
-installcask firefoxdeveloperedition
+for app in "${brew_apps[@]}"; do
+    echo "Установка $app..."
+    install_app "$app"
+done
 
-# editors
-installcask visual-studio-code
+# zsh
+brew install zsh
 
-# devtools
-installcask iterm2
-installcask filezilla
-
-# install powerline fonts
-mkdir ~/tmp && cd ~/tmp
-git clone https://github.com/powerline/fonts.git
-cd fonts
-sh -c ./install.sh
-cd ~/tmp && rm -rf ./fonts
-
-# Fira Code: monospaced font with programming ligatures
-# https://github.com/tonsky/FiraCode/wiki/VS-Code-Instructions
-brew tap caskroom/fonts
-installcask font-fira-code
-
-# Install Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Set zsh as default shell
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /bin/zsh
 
-# install extension for VS Code
-function installvs() {
-  code --install-extension "${@}" 2> /dev/null
-}
-
-# https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5
-installvs emmanuelbeziat.vscode-great-icons 
-installvs dracula-theme.theme-dracula
-installvs CoenraadS.bracket-pair-colorizer
-installvs oderwat.indent-rainbow
-installvs esbenp.prettier-vscode
-installvs WallabyJs.quokka-vscode
-installvs xabikos.JavaScriptSnippets # ES6
-installvs humao.rest-client
-installvs formulahendry.auto-close-tag
-installvs formulahendry.auto-rename-tag
-installvs SirTori.indenticator
-installvs abusaidm.html-snippets # HTML5
+# powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
